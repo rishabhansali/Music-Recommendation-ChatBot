9d2a4800cad38238e17e74b521ac50ec3db00457
rishabhansali <17bmiit@gmail.com>
2021-05-27 01:05:15 +0530

    updates

--- schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists users;
-    create table users (
-    id integer primary key autoincrement,
-    name varchar(50) not null,
-    username varchar(255) not null,
-    password varchar(50) not null
-);
